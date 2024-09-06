9387a9461d6b36cde597282681a97d3090691f11
faradev93 <faramarz.rezakhanlou.sf@gmail.com>
2024-09-06 16:38:38 +0330

    functions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let firstname = "faramarz"/2;
+let firstname = "faramarz" / 2;
 console.log(firstname);
 
 firstname = "masoud";
@@ -11,18 +11,35 @@ var job = "Programmer" + 8;
 let age = 30 + 18;
 
 let male = true;
-let feriche=null;
-let goje= false;
+let feriche = null;
+let goje = false;
 
 console.log(job);
 console.log(typeof male);
 
 
-let a=5;
-let b=6;
-let sum =a+b;
+let a = 5;
+let b = 6;
+let sum = a + b;
 console.log(sum);
 
-function sum(a,b){
-    console.log(a+b);
+// function sum1(a,b){
+//     console.log(a+b);
+// }
+// sum1(5,10);
+// sum1(15,20);
+// sum1(5,254);
+// sum1("faramarz ",1372);
+
+// function senn (xx,yy){
+// console.log(xx,yy);
+// }
+
+// let age2=Number (prompt ("Enter Your Ageeee??"));
+// senn(age2,5);
+function zarb() {
+    let a = Number(prompt("sennet?"));
+    let b = Number(prompt("zarb bar chand konam?"));
+    console.log(a * b);
 }
+zarb();
