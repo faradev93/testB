20b095779348936badb06f467ae02e21a4f54783
faradev93 <faramarz.rezakhanlou.sf@gmail.com>
2024-09-06 16:06:09 +0330

    delete spam func

--- script.js
+++ script.js
@@ -23,29 +23,6 @@ let b=6;
 let sum =a+b;
 console.log(sum);
 
-function sumi(){
-    let a=50;
-    let b=100;
+function sum(a,b){
     console.log(a+b);
 }
-sumi();
-
-function sumichar(){
-    let a="ferii ";
-    let b="khanloieee";
-    console.log(a+b);
-}
-sumichar();
-sumi();
-function sumi(a,b){
-    
-    console.log(a+b);
-}
-sumi();
-//-------------------------------------test-----------------//
-
-// function sum(a, b) {
-//     a+b
-
-// }
-// console.log(sum);
